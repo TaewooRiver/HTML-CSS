e8ce36abc73e98325394565e174f962d085ac6f1
Taewoo Kang <cooltwk@naver.com>
2021-09-26 12:12:29 +0000

    daily commit 0926

--- javascript/test.js
+++ javascript/test.js
@@ -6,7 +6,20 @@ function Item(name, price){
     }
 }
 
+function myClass(para1, para2){
+    return {
+        [para1] : para2,
+        [1+2+3] : 33,
+    }
+}
+
 let item1 = new Item("Doll", 30);
 console.log(item1);
 
 item1.showPrice();
+
+let object1 = new myClass("key1", "val1");
+console.log(object1);
+console.log(Object.keys(object1),
+Object.values(object1),
+Object.entries(object1))
