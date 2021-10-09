921a0e52511d255a4e836c2eed177246a9b62aea
Taewoo Kang <cooltwk@naver.com>
2021-10-09 14:29:58 +0000

    1009 commit

---
+++ javascript/methods/Array/ArrGen.js
@@ -0,0 +1,11 @@
+
+function AscendingRandomArray(numbers){
+    this.array = [];
+    for(let i = 0; i < numbers; ++i){
+        this.array.push(Math.round(Math.random() * 100));
+    }
+    this.array.sort((a, b) => a - b);
+}
+
+let array1 = new AscendingRandomArray(6)
+console.log(array1.array);
---
+++ javascript/methods/Array/splice.js
@@ -0,0 +1,1 @@
+//the .splice(m, n) method returns a leftover array where n characters starting from index m are removed.
