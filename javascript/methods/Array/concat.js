9c7bc76d3cc4cf8170fb75ea6cbbaf0c4a73cf72
Taewoo Kang <cooltwk@naver.com>
2021-10-12 13:32:17 +0000

    n1012 initial commit

--- javascript/methods/Array/concat.js
+++ javascript/methods/Array/concat.js
@@ -7,3 +7,6 @@ let arr2 = [4, 5, 6];
 let newArr = arr1.concat(arr2, 7, 8, 9, 0);
 console.log(newArr); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
+//or
+arr1 = [...arr1, ...arr2]
+console.log(arr1); // [1, 2, 3, 4, 5, 6]
