ebbd076f6eda4d606cbdcd6af665c1952d5a203e
Taewoo Kang <cooltwk@naver.com>
2021-10-23 13:34:38 +0000

    pseudo-TODO list for new toy project

---
+++ FunnyFunny.js
@@ -0,0 +1,18 @@
+/*
+FUNCTIONALITY: CREATES AN EXCEL FILE that 
+
+indicates whether a person is on vacation, taking a day off, is on guard duty, or is present. 
+
+The process should exclude(or minimize) a user's typing activity.
+
+This means that there should be a GUI(mainly execution buttons) for reducing the user's redundant actions.
+(by redundant actions, I mean inputting names, ranks, and status)
+
+METHOD 1. 
+Create an Excel program that edits a sheet.
+
+METHOD 2.
+Create a javascript program that prints out an html file with the same format as the Excel one.
+
+
+*/
